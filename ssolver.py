d0150315c323d4ee5c7554fd5c5c6c7e085dd2e2
Nia Maheshwari <unikorn.nkm31@gmail.com>
2021-04-11 14:13:19 -0400

    Debugged 'get_item' function and wrote a code that prints numbers from the sudoku puzzle in a diagonal manner across the screen. The code remains unfinished, however.

--- ssolver.py
+++ ssolver.py
@@ -1,14 +1,19 @@
+import time
+
 puzzle = open("in1.txt", "r")
+l = []
+for line in puzzle:
+    l.append(line)
+puzzle.close()
 
 def get_item(r,c):
-
-    l = []
-    for line in puzzle:
-        l.append(line)
-
+    r -= 1
+    c -= 1
     num = l[r].split()
 
     return num[c] 
 
-
-print(get_item(7,4))
+row = 1
+while row <= 9:
+    print(get_item(row,row))
+    row += 1
